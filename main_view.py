b821d70ba2a45c8d4969c825d0949efb2ada3fcd
SHIROSHIM <118763382+SHIROSHIM@users.noreply.github.com>
2022-12-21 15:46:39 +0600

    Create main_view.py

---
+++ main_view.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from .models import Product
+
+def main_view(request):
+    return render(request, 'layouts/main.html')
+
+def products_view(request):
+    products = Product.objects.all()
+    return render(request, 'products/products.html', {'products': products})
+
+  
+  
+  #products_view
+  from django.shortcuts import render
+from .models import Product
+
+def main_view(request):
+    return render(request, 'layouts/main.html')
+
+def products_view(request):
+    products = Product.objects.all()
+    return render(request, 'products/products.html', {'products': products})
